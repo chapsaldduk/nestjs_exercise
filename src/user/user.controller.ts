a76f331812c398a3776f9eba3064a51fe805a215
chapsaldduk <koala031532@gmail.com>
2023-03-30 07:08:32 +0900

    modified: controller functions type

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,37 +13,47 @@ import { UpdateUserDTO } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
+type funcs = {
+  getAll: string;
+  search: (searchingData: string) => string;
+  getOne: (userID: number) => Promise<User | undefined>;
+  create: (createUserData: CreateUserDTO) => Promise<User>;
+  remove: (id: number) => Promise<void>;
+  patch: (ID: string, updateData: UpdateUserDTO) => any;
+};
+
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getAll() {
+  getAll(): Promise<User[]> {
     return this.userService.getAll();
   }
 
+  // !complete
   @Get('/search')
-  search(@Query('user_id') searchingData: string) {
+  search(@Query('user_id') searchingData: string): string {
     return `We are searching for a data: ${searchingData}`;
   }
 
   @Get('/:id')
-  getOne(@Param('id') userID: number) {
+  getOne(@Param('id') userID: number): Promise<User | undefined> {
     return this.userService.getOne(userID);
   }
 
   @Post()
-  create(@Body() createUserData: CreateUserDTO) {
+  create(@Body() createUserData: CreateUserDTO): Promise<User> {
     return this.userService.create(createUserData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') id: number) {
+  remove(@Param('id') id: number): Promise<void> {
     return this.userService.deleteOne(id);
   }
 
   @Patch('/:id')
-  patch(@Param('id') ID: string, @Body() updateData: UpdateUserDTO) {
+  patch(@Param('id') ID: string, @Body() updateData: UpdateUserDTO): any {
     return {
       updateTarget: ID,
       ...updateData,
